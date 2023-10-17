b81247460da02217a4be2565423f5c25a4dbad5e
Mykola Fedotov <fedotovns@gmail.com>
2023-10-17 18:36:13 +0200

    task02 -done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+      `
+    )
+    .join("");
+  console.log(markup);
+  return list.insertAdjacentHTML("beforeend", markup);
+}
+createMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
