4119f78194f17bdea210772e2b02469a6a73c325
Mykola Fedotov <fedotovns@gmail.com>
2023-10-17 16:45:59 +0200

    task01 -done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,11 @@ const list = document.querySelector(".gallery");
 
 list.addEventListener("click", onClick);
 
+let instance;
+
 function onClick(e) {
+  e.preventDefault();
+
   if (!e.target.classList.contains("gallery-image")) {
     return;
   }
@@ -12,17 +16,23 @@ function onClick(e) {
   const bigImage = e.target.dataset.original;
   const altImg = e.target.alt;
 
-  const instance = basicLightbox.create(
-    `<div><img src="${bigImage}" alt="${altImg}" width="800"></div>`
-  );
-
-  instance.show();
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      instance.close();
+  instance = basicLightbox.create(
+    `<div><img src="${bigImage}" alt="${altImg}" width="800"></div>`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEsc);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEsc);
+      },
     }
-  });
+  );
+  instance.show();
+}
+
+function onEsc(e) {
+  if (e.code !== "Escape") return;
+  instance.close();
 }
 
 function createMarkup(arr) {
@@ -31,8 +41,10 @@ function createMarkup(arr) {
       ({ preview, original, description }) =>
         `
     <li class="gallery-item">
+    <a class="gallery-link" href="large-image.jpg">
       <img class="gallery-image" alt="${description}" src="${preview}" data-original="${original}" width="350">
-    </li>
+     </a>
+      </li>
   `
     )
     .join("");
@@ -41,5 +53,3 @@ function createMarkup(arr) {
 }
 
 createMarkup(galleryItems);
-
-// console.log(galleryItems);
