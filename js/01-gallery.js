8430c12ebe4a43b1212d1da3d6b8f8d0b945bbf2
Mykola Fedotov <fedotovns@gmail.com>
2023-10-17 15:39:51 +0200

    task01 -done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+list.addEventListener("click", onClick);
+
+function onClick(e) {
+  if (!e.target.classList.contains("gallery-image")) {
+    return;
+  }
+
+  const bigImage = e.target.dataset.original;
+  const altImg = e.target.alt;
+
+  const instance = basicLightbox.create(
+    `<div><img src="${bigImage}" alt="${altImg}" width="800"></div>`
+  );
+
+  instance.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li class="gallery-item">
+      <img class="gallery-image" alt="${description}" src="${preview}" data-original="${original}" width="350">
+    </li>
+  `
+    )
+    .join("");
+
+  return list.insertAdjacentHTML("beforeend", markup);
+}
+
+createMarkup(galleryItems);
+
+// console.log(galleryItems);
